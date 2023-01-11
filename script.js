99e78ab317702f000a5fed379556f24ae2432a29
Moberly3 <khachaturyan@spsejecna.cz>
2023-01-11 22:10:02 +0100

    js commit

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function Clock(){
     var seconds = date.getSeconds();
     var minutes = date.getMinutes();
     var hours = date.getHours();
-    var backgrnd = document.querySelector("body");
 
     if(10 >= seconds){
         seconds = "0" + seconds;
@@ -17,12 +16,12 @@ function Clock(){
         hours = "0" + hours;
     }
 
-    var clockBackground = "#" + seconds + hours + minutes;
-    var clockBorder = "#" + hours + minutes + seconds;
+    var clockBackground = "#" + seconds + minutes + hours;
+    var clockLine = "#" + hours + minutes + seconds;
 
-    backgrnd.style.backgroundColor = "#"+clockBackground;
     document.getElementById('clock').innerHTML = clockBackground;
     document.body.style.backgroundColor = clockBackground;
+    document.body.style.text = clockLine;
     setTimeout(Clock, 1000);
 }
 Clock();
