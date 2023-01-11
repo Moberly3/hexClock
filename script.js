6a22aefde3a86de07a6636d14194e29ea4be7152
Moberly3 <khachaturyan@spsejecna.cz>
2023-01-11 21:53:33 +0100

    js and html style commit

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ function Time(){
     var seconds = date.getSeconds();
     var minutes = date.getMinutes();
     var hours = date.getHours();
+    var clockBackground = "#" + seconds + hours + minutes;
+    var clockBorder = "#" + hours + minutes + seconds;
+
+    if(10 >= seconds){
+        seconds = "0" + seconds;
+    }
+
+    if(10 >= minutes){
+        minutes = "0" + minutes;
+    }
+
+    if(10 >= hours){
+        hours = "0" + hours;
+    }
+
     
     
 }
